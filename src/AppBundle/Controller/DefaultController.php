3ceed516336d69ea58c6ca248ffb3d0a01e7f31c
Fab <fabian.santer@hotmail.fr>
2019-04-24 11:20:20 +0200

    twig + exo

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -54,4 +54,31 @@ class DefaultController extends Controller
         $templateHtml = $this->renderView('default/page_test_template.html.twig');
         return new Response($templateHtml);
     }
+
+    /**
+     * @Route("/template-heritage", name="template_heritage")
+     */
+    public function templateHeritageAction(Request $request)
+    {
+        $templateHtml = $this->renderView('default/template_heritage.html.twig');
+        return new Response($templateHtml);
+    }
+
+    /**
+     * @Route("/page-avec-variable", name="page_avec_variable")
+     */
+    public function pageAvecVariableAction(Request $request)
+    {
+        $i = 0;
+        $i = $i + 15 * 2;
+
+        // passer des variables à twig pour pouvoir
+        // les utiliser dans le template : ici on a passé deux paramètres
+        // i et cle2, qui auront pour valeur 30 et 50 dans le template
+        $templateHtml = $this->renderView(
+            'default/toto_variable.html.twig',
+            ["i" => $i, 'cle2' => "50"]
+        );
+        return new Response($templateHtml);
+    }
 }
---
+++ src/AppBundle/Controller/ExoController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ExoController extends Controller
+{
+    /*
+     * Créer trois pages : routes, controller, et templates
+     * Chaque template doit hériter du template base.html.twig
+     * Et dans le body de chaque page, vous mettez :
+     * pour la page 1 : "Ceci est la page 1"
+     * pour la page 2 : "Ceci est la page 2"
+     * pour la page 3 : "Ceci est la page 3"
+     * Et dans chaqe template : vous mettez les trois liens qui pointent
+     * vers chacune des pages
+     * Dans la page 3, le controller doit faire une multiplication
+     * et passer au template le résultat de cette multiplication pour l'afficher
+     */
+}
