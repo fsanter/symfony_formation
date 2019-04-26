8929f51170655f133bd373787f253409f18af7d6
Fab <fabian.santer@hotmail.fr>
2019-04-26 11:43:16 +0200

    twig + twigextension

--- src/AppBundle/Controller/Exo2Controller.php
+++ src/AppBundle/Controller/Exo2Controller.php
@@ -16,6 +16,26 @@ class Exo2Controller extends Controller
         L'url est dynamique et le paramètre chiffre doit être récupéré dans le controller
         Vous devez ensuite créer la table de multiplication de ce chiffre dans un tableau PHP
         Vous devez afficher cette table dans un tableau html dans un nouveau template
+        le chiffre 5
+        0 * 5 = 0
+        1 * 5 = 5
+        2 * 5 = 10
+        Rajouter un h1 : Table de multiplication de "chiffre"
      */
+
+    /**
+     * @Route("/multiplication/{chiffre}", name="page_multiplication")
+     */
+    public function  multiAction(Request $request)
+    {
+        // faire les calculs et mettre dans un tableau
+        $tableMultiplication = [];
+
+        // passer ce tableau au template
+        $templateHtml = $this->render('', [
+
+        ]);
+        return $templateHtml;
+    }
 
 }
