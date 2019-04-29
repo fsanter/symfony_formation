d60fcc3630696242ef4a18ce4527eb088bae37b4
Fab <fabian.santer@hotmail.fr>
2019-04-29 10:33:50 +0200

    controller article avec doctrine

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use AppBundle\Entity\Article;
 
@@ -14,11 +15,54 @@ class ArticleController extends Controller
      * @Route("/article/view", name="article_view")
      */
     public function viewAction() {
+        /*
         $article = new Article();
         $article->setTitle("Titre de l'article 1");
         $article->setDescription("Ici c'est la description de le produit");
         $article->setIsOnline(true);
         $article->setCreatedAt(new \DateTime());
+        */
+
+        // récupérer un seul article depuis la base de données
+        $em = $this->getDoctrine()->getManager();
+        $article = $em->getRepository("AppBundle:Article")->find(2);
+
+        // générer une page d'erreur 404 si l'article n'existe pas
+        if ($article == null) {
+            // le code s'arrêtera ici si on rentre dans le if
+            throw new NotFoundHttpException();
+        }
+
+        $response = $this->render('article/view.html.twig', [
+            'article' => $article
+        ]);
+        return $response;
+    }
+
+    /**
+     * @Route("/article/view/{id}", name="article_view_url")
+     */
+    public function viewUrlAction($id) {
+        // récupérer un seul article depuis la base de données
+        $em = $this->getDoctrine()->getManager();
+        $article = $em->getRepository("AppBundle:Article")->find($id);
+
+        /*
+         * Chercher un article en fonction de critères
+         * on peut rechercher sur les propriétés de l'entité Article
+         * (title, description, isOnline, createdAt)
+         */
+        /*
+        $article = $em->getRepository("AppBundle:Article")->findOneBy([
+            'title' => 'Article titre'
+        ]);
+        */
+
+        // générer une page d'erreur 404 si l'article n'existe pas
+        if ($article == null) {
+            // le code s'arrêtera ici si on rentre dans le if
+            throw new NotFoundHttpException();
+        }
 
         $response = $this->render('article/view.html.twig', [
             'article' => $article
@@ -64,6 +108,40 @@ class ArticleController extends Controller
             'articlesOnline' => $articlesOnline
         ]);
         return $response;
+    }
+
+    /**
+     * @Route("/update/{id}", name="article_update")
+     */
+    public function updateAction($id) {
+        // récupérer un seul article depuis la base de données
+        $em = $this->getDoctrine()->getManager();
+        $article = $em->getRepository("AppBundle:Article")->find($id);
+
+        $article->setTitle("Nouveau titre");
+
+        $em->persist($article);
+        $em->flush();
+
+        return new Response("Article bien modifié");
+    }
+
+    /**
+     * @Route("/remove/{id}", name="article_remove")
+     */
+    public function removeAction($id) {
+        // récupérer un seul article depuis la base de données
+        $em = $this->getDoctrine()->getManager();
+        $article = $em->getRepository("AppBundle:Article")->find($id);
+
+        // supprimer une entité
+        if ($article != null) {
+            $em->remove($article);
+            $em->flush();
+        }
+
+
+        return new Response("Article bien supprimé");
     }
 
 }
